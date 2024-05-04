77bf446f3ca6efc857547631d33821be96859855
yulia yurieva <cryinghostt.56@gmail.com>
2024-05-04 21:33:25 +0300

    init project

---
+++ src/main/java/com/example/artspectrum/ArtspectrumApplication.java
@@ -0,0 +1,13 @@
+package com.example.artspectrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtspectrumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtspectrumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/artspectrum/ArtspectrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.artspectrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtspectrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
